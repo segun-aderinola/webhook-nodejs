1c7af69fa6ad075b1a15e3ba0b8e9049ee5ca9bd
SEGUN ADERINOLA <SEGUN.ADERINOLA@fundall.io>
2023-09-22 10:16:35 +0100

    webhook updated

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
 
 app.post('/webhook', (req, res)=>{
     
     const response = req.body;
 
     console.log('Received Webhook payload');
-    console.log(req);
+    console.log(req.body);
     res.status(200).json({'success': {'message':'Webhook received', 'data':response} });
 })
 
 
-const port = 3010;
-app.listen(port, console.log('Server Running on port'+port));
+const port = process.env.PORT || 3010;
+app.listen(port, console.log('Server Running on port '+port));
